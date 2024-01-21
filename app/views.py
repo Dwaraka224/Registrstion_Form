bafb80b6c973c0f3b933d8dddab312eccb051f60
Dwaraka224 <reddydwarakanath800@gmail.com>
2024-01-21 12:53:26 +0530

    Added media folder

--- app/views.py
+++ app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 from app.forms import *
 def registration(request):
     ufo=userForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
+    if request.method=='POST' and request.FILES:
+        ufd=userForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+           MUFDO=ufd.save(commit=False)
+           pw=ufd.cleaned_data['password']
+           MUFDO.set_password(pw)
+           MUFDO.save()
+           MPFDO=pfd.save(commit=False)
+           MPFDO.username=MUFDO
+           MPFDO.save()
+
+           return HttpResponse('Registration is successfully')
+        else:
+           return HttpResponse('Registration is Invalid')
+
     
     return render(request,'registration.html',d)
  

bed254a3c4425f0d9db5bcafeb375bbf086cf736
Dwaraka224 <reddydwarakanath800@gmail.com>
2024-01-23 21:52:50 +0530

    Created login page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.urls import reverse
+from django.contrib.auth import authenticate,login
 # Create your views here.
 from app.forms import *
 def registration(request):
@@ -30,5 +32,24 @@ def registration(request):
 
     
     return render(request,'registration.html',d)
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+    return render(request,'user_login.html')
  
  
